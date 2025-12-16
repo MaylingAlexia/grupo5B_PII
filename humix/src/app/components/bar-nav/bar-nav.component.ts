bea6434066bedcde7e6d3471901d39d85bf7b499
Ana G <ana.gutierrez.moncada@alumnos.uta.cl>
2025-12-16 13:07:46 -0300

    feat: actualizar componente bar-nav

--- humix/src/app/components/bar-nav/bar-nav.component.ts
+++ humix/src/app/components/bar-nav/bar-nav.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-bar-nav',
   templateUrl: './bar-nav.component.html',
-  styleUrls: ['./bar-nav.component.css']
+  styleUrls: ['./bar-nav.component.css'],
 })
 export class BarNavComponent {
+  isCollapsed: boolean = false;
+  @Output() sidebarToggle = new EventEmitter<boolean>();
 
+  constructor() {}
+
+  toggleSidebar(): void {
+    this.isCollapsed = !this.isCollapsed;
+    this.sidebarToggle.emit(this.isCollapsed);
+  }
 }
